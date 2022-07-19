0956da7302cf649ea00273c5955a95c366423959
Farhan <xpalmas@gmail.com>
2022-07-20 01:51:19 +0600

    add-changes

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BsTwitter, BsLinkedin} from 'react-icons/bs'
+import { FaFacebookF} from 'react-icons/fa'
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
+    </div>
+    <div>
+      <FaFacebookF />
+    </div>
+    <div>
+      <BsLinkedin />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
