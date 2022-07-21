8b8ed89baf5f4175bb9f1e6cb781c71fb357bb02
Farhan <xpalmas@gmail.com>
2022-07-21 22:00:27 +0600

    final

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { BsTwitter, BsLinkedin} from 'react-icons/bs'
+import { BsTwitter, BsLinkedin, BsGithub} from 'react-icons/bs'
 import { FaFacebookF} from 'react-icons/fa'
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href='https://twitter.com/FarhanAlmas4'> <BsTwitter/> </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href='https://www.facebook.com/black.sight/'> <FaFacebookF/> </a> 
     </div>
     <div>
-      <BsLinkedin />
+    <a href="https://www.linkedin.com/in/farhan-almas/"> <BsLinkedin/> </a>
+    </div>
+    <div>
+    <a href="https://github.com/xpalmas"> <BsGithub/> </a>
     </div>
   </div>
 );
